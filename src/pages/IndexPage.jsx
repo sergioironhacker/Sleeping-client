f8d8052f88bdb6c38b46098aeb0b75310f1c7269
Sergio Esteban <Sergioss772022@gmail.com>
2024-05-16 01:16:20 +0200

    json update

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,34 +1,28 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import Image from "../Image.jsx";
+import placesData from "./places.json"; // Importar el JSON directamente
 
 export default function IndexPage() {
-  const [places, setPlaces] = useState([]);
-
-  useEffect(() => {
-    axios.get('/places').then(response => {
-      setPlaces(response.data);
-    });
-  }, []);
+  const [places, setPlaces] = useState(placesData); // Utilizar el JSON importado como estado
 
   return (
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
       {places.length > 0 && places.map(place => (
-        <Link to={'/place/'+place._id} key={place._id}>
-          <div className="bg-gray-500 mb-2 rounded-2xl flex">
-            {place.photos?.[0] && (
-              <Image className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt=""/>
-            )}
-          </div>
-          <h2 className="font-bold">{place.address}</h2>
-          <h3 className="text-sm text-gray-500">{place.title}</h3>
-          <div className="mt-1">
-            <span className="font-bold">${place.price}</span> per night
+        <Link to="/register" key={place._id}> {/* Redirigir a la página de registro al hacer clic */}
+          <div>
+            <div className="bg-gray-500 mb-2 rounded-2xl flex">
+              {place.photos?.[0] && (
+                <img className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt=""/>
+              )}
+            </div>
+            <h2 className="font-bold">{place.address}</h2>
+            <h3 className="text-sm text-gray-500">{place.title}</h3>
+            <div className="mt-1">
+              <span className="font-bold">${place.price}</span> per night
+            </div>
           </div>
         </Link>
       ))}
     </div>
   );
 }
-
